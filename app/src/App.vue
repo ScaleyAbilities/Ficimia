759a758a990af4c60f780c3406b29693e560e3fe
Michael Baart <michael.baart96@gmail.com>
2019-03-29 22:25:03 -0700

    Add command requests

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,28 +1,113 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 v-show=!hide>Login</h1><h1 v-show=hide>Enter Command</h1>
+    <input v-show=hide v-model="command" placeholder="Enter command"> <br>
+    <!-- <textarea v-show=hide v-model="command" placeholder="Enter command"></textarea> <br> -->
+    <button v-show=hide v-on:click="execute()">Execute command</button>
+    <input v-show=!hide v-model="username" placeholder="John Appleseed"> <br>
+    <button v-show=!hide v-on:click="hide = !hide">Login</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  url: 'http://127.0.0.1:80/api/command',
+  data() {
+    return {
+      hide: false,
+      username: '',
+      command: ''
+    }
+  },
+  methods: {
+    execute() {
+      var cmd = this.command.toUpperCase().split(" ");
+      var data;
+      if(cmd[0] == "COMMIT_BUY" || cmd[0] == "CANCEL_BUY" || cmd[0] == "COMMIT_SELL" || cmd[0] == "CANCEL_SELL" || cmd[0] == "DISPLAY_SUMMARY"){
+        data = {
+          'cmd': cmd[0],
+          'usr': cmd[1],
+        }
+      } else if(cmd[0] == "ADD"){
+        data = {
+          'cmd': cmd[0],
+          'usr': cmd[1],
+          'params': {
+              'amount': cmd[2]
+          }
+        }
+      } else if(cmd[0] == "QUOTE" || cmd[0] == "CANCEL_SET_BUY" || cmd[0] == "CANCEL_SET_SELL"){
+        data = {
+          'cmd': cmd[0],
+          'usr': cmd[1],
+          'params': {
+              'stock': cmd[2]
+          }
+        }
+      } else if(cmd[0] == "BUY" || cmd[0] == "SELL" || cmd[0] == "SET_BUY_AMOUNT" || cmd[0] == "SET_SELL_AMOUNT"){
+        data = {
+          'cmd': cmd[0],
+          'usr': cmd[1],
+          'params': {
+              'stock': cmd[2],
+              'amount': cmd[3]
+          }
+        }
+      } else if(cmd[0] == "SET_BUY_TRIGGER" || cmd[0] == "SET_SELL_TRIGGER"){
+        data = {
+            'cmd': cmd[0],
+            'usr': cmd[1],
+            'params': {
+                'stock': cmd[2],
+                'price': cmd[3]
+            }
+        }
+      } else if(cmd[0] == "DUMPLOG"){
+        if(cmd.length == 2){
+          data = {
+            'cmd': cmd[0],
+            'usr': null,
+            'params': {
+                'filename': cmd[1]
+            }
+          }
+        } else if(cmd.length == 3)
+          data = {
+            'cmd': cmd[0],
+            'usr': cmd[1],
+            'params': {
+                'filename': cmd[2]
+            }
+          }
+      } else {
+        alert("Command not recognized")
+      }
+      if(data.cmd == "ADD" || data.cmd == "BUY" || data.cmd == "COMMIT_BUY" || data.cmd == "CANCEL_BUY" || data.cmd == "COMMIT_SELL" || data.cmd == "CANCEL_SELL" || data.cmd == "SET_BUY_AMOUNT" || data.cmd == "SET_BUY_TRIGGER" || data.cmd == "CANCEL_SET_BUY" || data.cmd == "SET_SELL_AMOUNT" || data.cmd == "SET_SELL_TRIGGER" || data.cmd == "CANCEL_SELL_TRIGGER"){
+          fetch(this.url, {
+          method: 'POST', // or 'PUT'
+          json: JSON.stringify(data), // data can be `string` or {object}!
+          headers:{
+            'Content-Type': 'application/json'
+          }
+        }).catch(error => console.error('Error:', error));
+      } else if(data.cmd == "QUOTE" || data.cmd == "DUMPLOG" || data.cmd == "DISPLAY_SUMMARY"){
+          fetch(this.url, {
+            method: 'GET',
+            json: JSON.stringify(data), // data can be `string` or {object}!
+            headers:{
+              'Content-Type': 'application/json'
+          }
+        })
+        .then(response => console.log('Responce:', response.json()))
+        .catch(error => console.error('Error:', error));
+      }
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
